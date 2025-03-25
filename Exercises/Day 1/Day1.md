266c1b11276e2704cc791ea7f87ecbe778897cd8
Josh <75248085+shua-shua-jo@users.noreply.github.com>
2025-03-25 18:21:59 +0800

    Day 1 Exercise

---
+++ Exercises/Day 1/Day1.md
@@ -0,0 +1,4 @@
+### Hands-On Exercises:
+
+- [x] Install Visual Studio and create a simple Console App.
+- [x] Write a program that takes user input and prints a message.
---
+++ Exercises/Day 1/Day1Exercise/Program.cs
@@ -0,0 +1,10 @@
+﻿// Program that takes user input and prints a message
+
+string? fname, lname;
+
+Console.WriteLine("What's your first name?");
+fname = Console.ReadLine();
+Console.WriteLine("What's your last name?");
+lname = Console.ReadLine();
+
+Console.WriteLine($"Hello {fname} {lname}, congrats on completing Day 1 of the C#|.NET Learning Plan.");
